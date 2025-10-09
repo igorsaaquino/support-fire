360b1ae658a1d79979ee1ef065f0af75d4ba97de
igorsalencar <igoralencar1985@gmail.com>
2025-10-08 22:44:49 -0300

    Projeto Inicializado

---
+++ app/src/main/java/com/example/supportfire/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.supportfire
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.supportfire.navigation.AppNavigation
+import com.example.supportfire.ui.theme.SupportFireTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SupportFireTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
